8fb311e8101bd6793228d13a27ca966b6096735a
Julia Gao Miller <julia.gao.miller@gmail.com>
2021-11-18 20:09:28 +0700

    successfully console logging the data that I want but not able to hold it in state

--- src/App.js
+++ src/App.js
@@ -7,36 +7,41 @@ const base = new Airtable({ apiKey: airtableAPIKey }).base(baseId);
 function App() {
 
   const [entries, setEntries] = useState([]);
+  const [total, setTotal] = useState(0);
 
-  let allEntries = []
+  let todaysEntries = []
   //this method pulls in the data
   useEffect(() => {
    //pull in the table view data from airtable
    base("entries")
      .select({ view: "Grid view" })
      .eachPage((records, fetchNextPage) => { 
-       console.log(records.length)
-       records.forEach((record) => { // check if the date
-        if(record.fields.Date === "2021-11-18"){
-          console.log(record.fields)
+       records.forEach((record) => { 
+        if(record.fields.Date === "2021-11-18"){ // check if the date is equal to today
+          let entry = {}
+          entry.amount = record.fields.Amount
+          setTotal(total + entry.amount)
+          entry.category = record.fields.Category
+          entry.date = record.fields.Date
+          entry.location = record.fields.Location
+          entry.description = record.fields.Description
+          entry.dollars = Math.round(record.fields.Dollars * 100) / 100 //Math.round(num * 100) / 100
+          console.log(entry)
+          todaysEntries.push(entry)
+          console.log(`todayz entry length: ${todaysEntries.length}`)
+          setEntries(todaysEntries)
         }
-         
        })
-      //  allEntries.push(records)
-      //  console.log(allEntries)
-      //  setEntries(allEntries)
-      //  setEntries(records);
-       // console.log(records)
        fetchNextPage();
      });
     //  console.log(`Total number of entries: ${allEntries.length}`)
-     
   }, []);
   
-  console.log(`Total number of entries: ${entries.length}`)
 
   return (
     <div className="App">
+      <p>Total number of entries: {entries.length} </p>
+      <p>Total = {total}</p>
     </div>
   );
 }
